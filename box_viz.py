ebc9c29900a2c8f6007263b7852974545b27c6e3
ChengBowen <chengbowen052@163.com>
2020-11-14 21:25:45 +0800

    fix bug

--- box_viz.py
+++ box_viz.py
@@ -3,7 +3,7 @@ import torch
 import os
 import sys
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-ROOT_DIR = os.path.dirname(BASE_DIR)
+ROOT_DIR = BASE_DIR
 sys.path.append(os.path.join(ROOT_DIR, 'utils'))
 import pc_util
 
