f0d4ecaf65de1284a13818297109f5073c25552e
ChengBowen <chengbowen052@163.com>
2020-11-15 04:25:38 +0800

    fix bug

--- box_viz.py
+++ box_viz.py
@@ -34,8 +34,8 @@ if not os.path.exists(dump_dir):
     os.mkdir(dump_dir)
 
 for name in all_scan_names:
-    scene_name = name[3:15]
-    if scene_name != 'scene0488_00':
+    scene_name = name[3:9]
+    if scene_name != '000001':
         continue
     obbs = np.load(os.path.join(FLAGS.path,name))
     print(obbs[:, -1])
