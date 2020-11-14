a12144ce4b2c9179b149e8219f4caa4447b77d71
ChengBowen <chengbowen052@163.com>
2020-11-14 21:14:46 +0800

    add box_viz.py

---
+++ box_viz.py
@@ -0,0 +1,43 @@
+import numpy as np
+import torch
+import os
+import sys
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.dirname(BASE_DIR)
+sys.path.append(os.path.join(ROOT_DIR, 'utils'))
+import pc_util
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--path', default=None)
+parser.add_argument('--dataset')
+FLAGS = parser.parse_args()
+
+all_scan_names = list(set([os.path.basename(x)
+                           for x in os.listdir(FLAGS.path) if x.endswith('npy')]))
+print(all_scan_names[0])
+
+if FLAGS.dataset == 'scannet':
+    n_class = 18
+elif FLAGS.dataset == 'sunrgbd':
+    n_class = 10
+else:
+    raise NotImplementedError
+
+dump_dir = 'dump_'+FLAGS.dataset
+dump_dir = os.path.join(ROOT_DIR, dump_dir)
+
+for name in all_scan_names:
+    scene_name = name[3:15]
+    obbs = np.load(name)
+    boxes = obbs[:, :-1]
+    sem_cls = obbs[:, -1] - 1
+    print('(%d, %d)' % (obbs.shape[0], obbs.shape[1]))
+    case_dump_dir = os.path.join(dump_dir, scene_name)
+    if not os.path.exists(case_dump_dir):
+        os.mkdir(case_dump_dir)
+    for l in np.unique(sem_cls):
+        mask = (sem_cls == l)
+        if np.sum(mask)>0:
+            pc_util.write_oriented_bbox(boxes[mask, :], os.path.join(case_dump_dir, '%d_pred_confident_nms_bbox.ply'%(l)))
+
